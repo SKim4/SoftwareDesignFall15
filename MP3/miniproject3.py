68736ce6940eca0131045bac4be3a4685f2d447c
soeun kim <kimsoun0107@kaist.ac.kr>
2015-10-30 14:47:50 -0400

    Final

--- MP3/miniproject3.py
+++ MP3/miniproject3.py
@@ -1,39 +1,18 @@
 import pygame
 from pygame.locals import *
-import time
-
-
-
-
-
-
-
-class Brick:
-    def __init__(self,screen):
-    	self.screen = screen
-        
-
-    def draw(self,color,x,y,height):
-        pygame.draw.rect(self.screen,color,pygame.Rect(x,y,90,height))
-
-
 
 
 
 
 class RythemgameModel:
     def __init__(self):
-        self.bricky1 = [180,110,-160,-410,-1440,-2480,-2640,-3015,-3300,-3565,-3630]
-        self.bricky2 = [-560,-810,-1090,-1330,-2400,-2670,-3140,-3840]
+        self.bricky1 = [180,110,-160,-410,-1440,-2480,-2640,-3015,-3300,-3565,-3630] #put brick1s in these coordinates
+        self.bricky2 = [-560,-810,-1090,-1330,-2400,-2670,-3140,-3840] #put brick2s in these coordinates
         self.bricky3 = [-590,-690,-780,-1110,-1220,-1600,-1850,-2240,-2860,-3500,-3705,-4455]
         self.bricky4 = [-620,-1150,-1640,-1720,-2110,-2200,-2700,-2880,-3300,-3735,-3940,-3980,-4525]
         self.bricky5 = [-1670,-2150,-2720,-2950,-3420,-3765,-4100,-4220,-4330,-4595]
-        self.brickx = [5,100,195,290,385]
-        self.brickcolor = [(50,60,130),(150,215,230),(255,175,200),(255,255,130),(180,230,30)]
-
-
-
-
+        self.brickx = [5,100,195,290,385] # brick1s to brick5s is in these x coordinates
+        self.brickcolor = [(50,60,130),(150,215,230),(255,175,200),(255,255,130),(180,230,30)] # brick1s to brick5s have colors in these orders.
 
 
 
@@ -45,78 +24,80 @@ class RythemgameWindowView:
 
 
     def draw(self):
-        nightsky = pygame.image.load("nightsky2.jpg")
-        self.screen.fill(pygame.Color(0,0,0))
-        self.screen.blit(nightsky,(0,0))
+        # background image
+        nightsky = pygame.image.load("nightsky2.jpg") 
+        screen.fill(pygame.Color(0,0,0))
+        screen.blit(nightsky,(0,0))
 
+        # view score on the screen
         view.score(str(score))
 
 
-
+        # draw background bricks on the bottom of screen
         for i in range(5):
-            pygame.draw.rect(self.screen,model.brickcolor[i],pygame.Rect(model.brickx[i],y2,90,40))
+            pygame.draw.rect(screen,model.brickcolor[i],pygame.Rect(model.brickx[i],y2,90,40))
 
+        # draw lines between bricks
         for i in range(4):
-            pygame.draw.line(self.screen,(255,255,255),(model.brickx[i+1]-3,40),(model.brickx[i+1]-3,680),2)
+            pygame.draw.line(screen,(255,255,255),(model.brickx[i+1]-3,40),(model.brickx[i+1]-3,680),2)
 
-
+        # move brick1s below as time passes
         for i in range(len(model.bricky1)):
-            view.movingbrick(model.brickcolor[0],x1,model.bricky1[i]+dy)
+            view.movingbrick(model.brickcolor[0],model.brickx[0],model.bricky1[i]+dy)
 
+        # move brick2s below as time passes
         for i in range(len(model.bricky2)):
-            view.movingbrick(model.brickcolor[1],x2,model.bricky2[i]+dy)
+            view.movingbrick(model.brickcolor[1],model.brickx[1],model.bricky2[i]+dy)
 
         for i in range(len(model.bricky3)):
-            view.movingbrick(model.brickcolor[2],x3,model.bricky3[i]+dy)
+            view.movingbrick(model.brickcolor[2],model.brickx[2],model.bricky3[i]+dy)
 
         for i in range(len(model.bricky4)):
-            view.movingbrick(model.brickcolor[3],x4,model.bricky4[i]+dy)
+            view.movingbrick(model.brickcolor[3],model.brickx[3],model.bricky4[i]+dy)
 
         for i in range(len(model.bricky5)):
-            view.movingbrick(model.brickcolor[4],x5,model.bricky5[i]+dy)
+            view.movingbrick(model.brickcolor[4],model.brickx[4],model.bricky5[i]+dy)
             
 
 
 
-
+    # put brick on (x,y) coordinate with color
     def movingbrick(self,color,x,y):
-        return pygame.draw.rect(self.screen,color,(x,y,90,40))
+        pygame.draw.rect(screen,color,(x,y,90,40))
 
 
-
+    # write text about score on the right top of screen
     def score(self,score):
         font = pygame.font.SysFont("None",28)
         textsurface = font.render("SCORE : "+score, True, (255,170,0))
         textRect = textsurface.get_rect()
         textRect.center = (380,20)
-        self.screen.blit(textsurface,textRect)
+        screen.blit(textsurface,textRect)
 
 
+    # countdown numbers from 3 to 1 before game starting
     def first(self,number):
-        self.screen.fill(pygame.Color(0,0,0))
+        screen.fill(pygame.Color(0,0,0))
         font = pygame.font.SysFont("None",150)
         textsurface = font.render(number, True, (255,170,0))
         textRect = textsurface.get_rect()
         textRect.center = (240,320)
-        self.screen.blit(textsurface,textRect)
+        screen.blit(textsurface,textRect)
         pygame.display.update()
 
 
 
+    # view the final score after game finishing
     def final(self,score):
-        self.screen.fill(pygame.Color(0,0,0))
+        screen.fill(pygame.Color(0,0,0))
         font = pygame.font.SysFont("None",100)
         textsurface = font.render("score : " +score, True, (255,170,0))
         textRect = textsurface.get_rect()
         textRect.center = (240,320)
-        self.screen.blit(textsurface,textRect)
-    
+        screen.blit(textsurface,textRect)
         pygame.display.update()
 
         
-
-
-
 
 
 
@@ -129,14 +110,9 @@ class RythemgameController:
         if event.type == pygame.KEYDOWN:
             global score
             if event.key == pygame.K_f:
-                for i in range(len(model.bricky1)):
+                for i in range(len(model.bricky1)): # for all brick1s in model class, if player put f button when moving bricks are near the bottom bricks, give score +10
                     if y2-level+10 <= model.bricky1[i]+dy <= y2+level:
                         score += 10
-                A = USEREVENT+1
-                pygame.time.set_timer(A,1000)
-                if pygame.event.get(A):
-                    column.draw1()
-
             if event.key == pygame.K_g:
                 for i in range(len(model.bricky2)):
                     if y2-level+10 <= model.bricky2[i]+dy <= y2+level:
@@ -163,12 +139,14 @@ if __name__=='__main__':
     model = RythemgameModel()
     view = RythemgameWindowView(model,screen)
     controller = RythemgameController(model,screen)
-    Brick = Brick(screen)
+    
     
 
     
     
     running = True
+
+    # countdown before game starting
     view.first(str(3))
     pygame.time.delay(1000)
     view.first(str(2))
@@ -177,22 +155,21 @@ if __name__=='__main__':
     pygame.time.delay(1000)
 
     
-    x1 = 5
-    x2 = 100
-    x3 = 195
-    x4 = 290
-    x5 = 385
-    y1 = 40
+    # often used value
+    y1 = 40 
     y2 = 580
-    width = 90
-    height = 40
+
+
+    width = 90 # width of all bricks
+    height = 40 # height of all bricks
     dy = 0
-    score = 0
-    level = 20
+    score = 0 
+    level = 20 # as value is lower, game level is harder
 
     clock = pygame.time.Clock()
     FPS = 500
 
+    # insert music into the game
     file = 'IsntSheLovely.wav'
     pygame.mixer.init()
     pygame.mixer.music.load(file)
@@ -220,9 +197,11 @@ if __name__=='__main__':
         milliseconds = clock.tick(FPS)
         seconds = milliseconds / 1000.0
   
+        # to move bricks as time passes
         dy = dy + seconds*130
         
 
+        # after final brick is out of screen(game finishes), view final score screen
         if model.bricky5[9]+dy>=1000:
             view.final(str(score))
 
